646dfc09ecf638bcbda839f7a1a700f7a1683f43
nyrkjac <nyrkmusic@gmail.com>
2024-07-24 21:05:27 +0200

    only counts objects that are more than 5ms apart

--- InfraRed-Tacho.ino
+++ InfraRed-Tacho.ino
@@ -6,48 +6,54 @@ rgb_lcd lcd;
 
 const int IR = 4;
 // LCD-Backlight
-int colorR = 0;
-int colorG = 255;
-int colorB = 0;
+const int colorR = 0;
+const int colorG = 255;
+const int colorB = 0;
 // variables for time between objects
 unsigned long lastTime = 0;
 unsigned long currentTime = 0;
+const int amount = 6;  // choose how many spokes get averaged
+int timeList[amount];
 int counter = 0;
-int amountTimes = 5;
-int timeList[6] = {0, 0, 0, 0, 0, 0};
-int objects = 0; // counts total objects
+int objects = 0;  // counts total objects
 
-// stores times in a list and prints
-void addTime(){
-  objects++;
-  if(counter == amountTimes){
-    // this code runs every when the counter is at max
-    int total = 0;
-    for (byte i = 0; i < amountTimes; i++){
-      total = total + timeList[i];
-    }
-    float avg = (total/amountTimes)+0.5;
-    int avgRound = (int) avg;
-    Serial.println(avgRound); // print avg of last 5 times to the console
-    counter = 0;
+// initializes list by putting int 0 in each element
+void initList() {
+  for (byte i = 0; i < amount - 1; i++) {
+    timeList[i] = 0;
   }
-  else {
-    counter++;
-  }
-  // set time for next measurement
-  lastTime = currentTime;
 }
 
-void setup(){
+// stores times in a list and prints average every [amount] times
+void addTime() {
+  objects++;
+  if (counter == amount - 1) {
+    // this code runs every [amount] times
+    int total = 0;
+    for (byte i = 0; i < amount - 1; i++) {
+      total = total + timeList[i];
+    }
+    float avg = (total / amount) + 0.5;
+    int avgRound = (int)avg;
+    Serial.println(avgRound);  // print avg of last [amount] times to the console
+    counter = 0;
+  } else {
+    counter++;
+  }
+  lastTime = currentTime;  // set time for next measurement
+}
+
+void setup() {
   pinMode(IR, INPUT);
-  lcd.begin(16, 2); // for a 16 by 2 pixels LCD
+  lcd.begin(16, 2);  // for a 16 by 2 pixels LCD
   lcd.setRGB(colorR, colorG, colorB);
+  initList();
   Serial.begin(9600);
 }
 
-void loop(){
+void loop() {
   int receiverState = digitalRead(IR);
-  if (receiverState == LOW){
+  if (receiverState == LOW) {
     // this code executes if there is an object
     if (lastTime == 0) {
       // first object detected
@@ -56,8 +62,10 @@ void loop(){
       // second object detected
       currentTime = millis();
       unsigned long duration = currentTime - lastTime;
-      timeList[counter] = duration;
-      addTime();
+      if (duration > 5) {
+        timeList[counter] = duration;
+        addTime();
+      }
 
       // clear LCD and print new time
       lcd.clear();
@@ -68,13 +76,12 @@ void loop(){
       lcd.print(objects);
       lcd.print(" in total");
     }
-    while (digitalRead(IR) == LOW){
+    while (digitalRead(IR) == LOW) {
       // do nothing, just wait until the object is gone
     }
-  }
-  else {
+  } else {
     // this code executes when there is no object
-    while (digitalRead(IR) == HIGH){
+    while (digitalRead(IR) == HIGH) {
       // wait for object (this loop prevents console spam)
     }
   }
